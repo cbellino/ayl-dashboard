b2e3b801efed3eef1b60df507624795296ee50de
Colin Bellino <bellino.colin@gmail.com>
2016-07-15 23:55:50 +0200

    feat(instances): add a loader for the instances page

--- src/components/InstanceList/InstanceList.js
+++ src/components/InstanceList/InstanceList.js
@@ -1,22 +1,36 @@
 import React from 'react'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
+import RefreshIndicator from 'material-ui/RefreshIndicator'
 
 import s from './InstanceList.scss'
 import InstanceItemContainer from '../../containers/InstanceItemContainer'
 
 const InstanceList = ({ instances }) => {
 
-  const instanceNodes = instances.map((instance) =>
-    <InstanceItemContainer
-      key={instance.get('key')}
-      instance={instance}
-    />
+  const instanceNodes = () => (
+    instances.size > 0 ? (
+      instances.map((instance) =>
+        <InstanceItemContainer
+          key={instance.get('key')}
+          instance={instance}
+        />
+      )
+    ) : (
+      <div className={s.loader}>
+        <RefreshIndicator
+          size={96}
+          left={-48}
+          top={0}
+          status="loading"
+        />
+      </div>
+    )
   )
 
   return (
     <div className={s.root}>
-      {instanceNodes}
+      {instanceNodes()}
     </div>
   )
 }
